3d8f5e30d4dc859ecc49fc6e280861288b3cd8ef
MustafaRaheel26 <mustafaraheel26@gmail.com>
2024-02-29 09:56:45 -0800

    javascript

--- chapter3/app.js
+++ chapter3/app.js
@@ -5,8 +5,16 @@
 // Task2
 
 
+
 // Task3
 // var birthYear="2007";
 // document.write ("My birth year is " +birthYear+ "<br> Data type of my declared variable is number.")
 
 // Task4
+// var visitorName=prompt("Enter visitors name");
+// var productName=prompt("Enter product name");
+// var productQuantity=prompt("Enter product quantity");
+
+// document.write (visitorName +" ordered "+productQuantity+" "+productName+" from XYZ Clothing store");
+
+
